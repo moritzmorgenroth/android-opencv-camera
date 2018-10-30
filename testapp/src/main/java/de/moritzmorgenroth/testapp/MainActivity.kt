a9f156aa8d29bde915239d4ba20b14a3ddba42cc
Moritz Morgenroth <development@moritzmorgenroth.de>
2018-10-30 09:57:12 +0100

    Update dependencies and rebuild

--- testapp/src/main/java/de/moritzmorgenroth/testapp/MainActivity.kt
+++ testapp/src/main/java/de/moritzmorgenroth/testapp/MainActivity.kt
@@ -2,11 +2,14 @@ package de.moritzmorgenroth.testapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import de.moritzmorgenroth.opencvtest.RecognizerActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val a: RecognizerActivity
+
     }
 }
