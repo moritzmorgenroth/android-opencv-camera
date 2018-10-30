96e63876b908c7653280c8f768cce9b65cc0a52a
Moritz Morgenroth <development@moritzmorgenroth.de>
2018-10-30 09:58:36 +0100

    Star Recognizer Activity

--- testapp/src/main/java/de/moritzmorgenroth/testapp/MainActivity.kt
+++ testapp/src/main/java/de/moritzmorgenroth/testapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package de.moritzmorgenroth.testapp
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import de.moritzmorgenroth.opencvtest.RecognizerActivity
@@ -9,7 +10,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val a: RecognizerActivity
+        val intent = Intent(this, RecognizerActivity::class.java)
+        startActivity(intent)
 
     }
 }
